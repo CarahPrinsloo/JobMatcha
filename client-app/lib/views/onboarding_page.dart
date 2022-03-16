416c81b538137ffe929cb4afba90cfc356ee8245
Carah Prinsloo <cprinsloo@student.wethinkcode.co.za>
2022-03-16 16:08:38 +0200

    added form to signup page; need to still add bio and images input to sign up page

--- client-app/lib/views/onboarding_page.dart
+++ client-app/lib/views/onboarding_page.dart
@@ -1,3 +1,5 @@
+import 'package:client_app/views/home_page.dart';
+import 'package:dropdown_formfield/dropdown_formfield.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -28,7 +30,7 @@ class _OnboardingPageState extends State<OnboardingPage> {
           children: [
             page(Colors.deepPurpleAccent, "Page 1"),
             page(Colors.deepPurple, "Page 2"),
-            page(Colors.purple, "Page 3"),
+            const SignUpPage(),
           ],
         ),
       ),
@@ -54,8 +56,8 @@ class _OnboardingPageState extends State<OnboardingPage> {
             child: const Text("NEXT"),
             onPressed: () {
               controller.nextPage(
-                  duration: const Duration(milliseconds: 500),
-                  curve: Curves.easeOut,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeOut,
               );
             },
           ),
@@ -66,22 +68,202 @@ class _OnboardingPageState extends State<OnboardingPage> {
 
   SmoothPageIndicator pageIndicator() {
     return SmoothPageIndicator(
-              controller: controller,
-              count: 3,
-              onDotClicked: (index) => controller.animateToPage(
-                  index,
-                  duration: const Duration(milliseconds: 500),
-                  curve: Curves.easeIn,
-              ),
-            );
+      controller: controller,
+      count: 3,
+      onDotClicked: (index) => controller.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeIn,
+      ),
+    );
   }
 
   Container page(Color color, String text) {
     return Container(
-            color: color,
-            child: Center(
-              child: Text(text),
+      color: color,
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+}
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final formKey = GlobalKey<FormState>(); //key for form
+
+  late String email;
+  late String fullName;
+  late int age;
+  late String jobTitle;
+
+  @override
+  void initState() {
+    super.initState();
+    jobTitle = "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+
+    return Container(
+      padding: const EdgeInsets.only(left: 40, right: 40),
+      child: Form(
+        key: formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: height * 0.04),
+            const Text(
+              "WELCOME",
+              style: TextStyle(fontSize: 30, color: Colors.deepPurple),
             ),
-          );
+            SizedBox(height: height * 0.05),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "Enter your email address",
+              ),
+              validator: (value) {
+                if (value != null && value.isNotEmpty && _isValidEmail(value)) {
+                  email = value;
+                  return null;
+                }
+                email = "";
+                return "Required: Enter a valid email address";
+              },
+            ),
+            SizedBox(height: height * 0.05),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "Enter your full name",
+              ),
+              validator: (value) {
+                if (value != null && value.isNotEmpty) {
+                  fullName = value;
+                  return null;
+                }
+                fullName = "";
+                return "Required: Enter your full name";
+              },
+            ),
+            SizedBox(height: height * 0.05),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "Enter your age",
+              ),
+              validator: (value) {
+                if (value != null && value.isNotEmpty && _isInteger(value) && (int.parse(value) >= 1)) {
+                  age = int.parse(value);
+                  return null;
+                }
+                age = -1;
+                return "Required: Enter your age";
+              },
+            ),
+            SizedBox(height: height * 0.05),
+            jobTitleDropDownList(),
+            ElevatedButton(
+              onPressed: () async {
+                if (formKey.currentState!.validate()) {
+                  formKey.currentState!.save();
+                  // add user here w/ async method
+
+                  if (!(fullName == null || fullName.isEmpty)
+                  && !(email == null || email.isEmpty)
+                  && !(age == null && age <= 0)
+                  && !(jobTitle == null || jobTitle.isEmpty)) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                    );
+                  }
+                }
+              },
+              child: Text("Submit"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  DropDownFormField jobTitleDropDownList() {
+    return DropDownFormField(
+      titleText: 'Job Title',
+      hintText: 'Choose your job title',
+      value: jobTitle,
+      onSaved: (value) {
+        setState(() {
+          jobTitle = value;
+        });
+      },
+      onChanged: (value) {
+        setState(() {
+          jobTitle = value;
+        });
+      },
+      dataSource: const [
+        {
+          "display": "Full Stack Software Developer",
+          "value": "Full Stack Software Developer",
+        },
+        {
+          "display": "Backend Software Developer",
+          "value": "Backend Software Developer",
+        },
+        {
+          "display": "Frontend Software Developer",
+          "value": "Frontend Software Developer",
+        },
+        {
+          "display": "Web Developer",
+          "value": "Web Developer",
+        },
+        {
+          "display": "Data Scientist",
+          "value": "Data Scientist",
+        },
+        {
+          "display": "Mobile Developer",
+          "value": "Mobile Developer",
+        },
+        {
+          "display": "DevOps Developer",
+          "value": "DevOps Developer",
+        },
+      ],
+      textField: 'display',
+      valueField: 'value',
+      validator: (value) {
+        if (value != null && value.isNotEmpty) {
+          jobTitle = value;
+          return null;
+        }
+        jobTitle = "";
+        return "Required: Choose a job title";
+      },
+    );
+  }
+
+  bool _isValidEmail(String email) {
+    return RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email);
+  }
+
+  bool _isInteger(value) {
+    try {
+      double.parse(value);
+    } on FormatException {
+      return false;
+    }
+    return true;
   }
 }
